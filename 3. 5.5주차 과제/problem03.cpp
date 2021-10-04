5bbfdb98b20ceaacb67d0ea7623bbdd91197b904
green-study <soo010410@naver.com>
2021-10-05 03:38:30 +0900

    5.5주차 과제

---
+++ 3. 5.5주차 과제/problem03.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+class Animal {
+private:
+	std::string name;
+
+public:
+	Animal() {};
+	Animal(std::string name) : name(name) {};
+
+	void showName() {
+		std::cout << "Name is " << name << std::endl;
+	}
+
+	//operator+ 추가하세요
+	Animal operator+ (const Animal& a) {
+		return Animal(name + a.name);
+	}
+
+};
+
+int main() {
+
+	Animal cat("Nabi");
+	cat.showName();
+	Animal dog("Jindo");
+	dog.showName();
+
+	Animal catDog = dog + cat; //개냥이 합성
+	catDog.showName();
+
+	dog.showName();
+
+	getchar();
+
+	return 0;
+}
+
